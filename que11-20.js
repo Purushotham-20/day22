09508c34133356072b3bb11a5fc2069848bbe7b9
Purushotham-20 <puthatarak036@gmail.com>
2025-07-15 07:41:45 +0530

    Create que11-20.js

---
+++ que11-20.js
@@ -0,0 +1,21 @@
+let movie="Julayi";
+console.log(movie);
+let height=172;
+console.log(height);
+let isLoggedIn=false;
+console.log(isLoggedIn);
+let s=null;
+console.log(typeof s);
+const id=Symbol('id');
+console.log(id);
+const person = {
+    name:"purushotham",
+    age:19,
+    email:"purushotham185@gmail.com"
+};
+console.log(person);
+console.log(typeof null);
+console.log(typeof undefined);
+let arr=[1,2,3,4,5];
+console.log(typeof arr);
+console.log(Array.isArray(arr));
