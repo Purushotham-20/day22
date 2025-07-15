9d5f9ab08ae44a21a41aeb0f662d4bb80e7aa141
Purushotham-20 <puthatarak036@gmail.com>
2025-07-15 07:43:07 +0530

    Create que25-30.js

---
+++ que25-30.js
@@ -0,0 +1,24 @@
+console.log("Question 25");
+let a=10;
+let b=5;
+console.log("a > b:", a > b);
+console.log("a < b:", a < b);
+console.log("a >= b:", a >= b);
+console.log("a <= b:", a <= b);
+console.log("Question 26");
+let c='5';
+let d=5;
+console.log(c==d);
+console.log(c===d);
+// != checks whether both numbers are true or not
+// !== checks both number and type are true or not
+console.log("Question 28");
+let isUser=true;
+let isAdmin=false;
+console.log(isUser && isAdmin);
+console.log(isUser || isAdmin);
+console.log(!isUser);
+console.log("Question 29");
+let age=18;
+let n = (age>=18)? "Adult":"Minor";
+console.log(n);
